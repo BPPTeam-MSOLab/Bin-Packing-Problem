a6e703431fbb71c65407309437eeba7ec5c5f29b
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2024-07-28 15:36:18 +0700

    update

--- Data/Generator.py
+++ Data/Generator.py
@@ -7,23 +7,25 @@ from typing import List, Tuple
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 class Generator:
-    def __init__(self, n_items: int, seed: int = 0, bin_size: List[int] = [100, 100, 100], **kargs):
+    def __init__(self, n_items: int, n_bins: int = 1, seed: int = 0, bin_size: List[int] = [100, 100, 100], **kargs):
         """
         Parameters:
-        :param n_items: Number of items to generate
+        :param n_items: Number of items to generate for each bin
+        :param n_bins: Number of bins to generate
         :param seed: Seed for random number generator
         :param bin_size: Size of the bin in 3 dimensions
         :param n_samples: Number of samples to generate
 
         Note:
-        - We will generate n_items + n_samples items and then remove n_samples topmost items
+        - We will generate n_items + n_samples items for each bin and then remove n_samples topmost items
         - The remaining n_items items will be randomly reordered
         """
         self.n_items: int = n_items
+        self.n_bins: int = n_bins
         self.seed: int = seed
         self.bin_size: List[int] = bin_size
-        self.items: List[Tuple[List[int], List[int]]] = []
-        self.volume: int = bin_size[0] * bin_size[1] * bin_size[2]
+        self.items: List[List[Tuple[List[int], List[int]]]] = []
+        self.total_volume: int = 0
 
         if 'n_samples' in kargs:
             self.n_samples: int = kargs['n_samples']
@@ -35,68 +37,85 @@ class Generator:
 
     def generate(self) -> None:
         """
-        Generate random items and write them to a file.
-        - We will generate items by recursively splitting the bin into 2 parts along the largest dimension.
-        - We also keep track of the origin of each item (coordinates of the left-bottom-back corner) for visualization.
+        Generate random items for all bins and write them to a file.
         """
+        def generate_for_bin(bin_origin: List[int]) -> List[Tuple[List[int], List[int]]]:
+            """
+            Generate random items for a single bin.
+            - We will generate items by recursively splitting the bin into 2 parts along the largest dimension.
+            - We also keep track of the origin of each item (coordinates of the left-bottom-back corner) for visualization.
+            """
+            items = [(bin_origin, self.bin_size[:])]
+            bin_volume = self.bin_size[0] * self.bin_size[1] * self.bin_size[2]
+
+            for _ in range(self.n_items + self.n_samples - 1):
+                (origin, item) = items.pop()
+                
+                # Choose the dimension with the largest size to split
+                dimension: int = item.index(max(item))
+                size: int = item[dimension]
+                
+                if size == 1:
+                    items.append((origin, item))
+                    continue
+                
+                # Randomly choose a cut point
+                cut_point: int = random.randint(1, size - 1)
+                
+                # Create 2 new items after cutting
+                new_item1: List[int] = item[:]
+                new_item2: List[int] = item[:]
+                new_item1[dimension] = cut_point
+                new_item2[dimension] = size - cut_point
+                
+                # Create 2 new origins (coordinates of the left-bottom-back corner)
+                new_origin1: List[int] = origin[:]
+                new_origin2: List[int] = origin[:]
+                new_origin2[dimension] += cut_point
+                
+                # Add new items to the list
+                items.append((new_origin1, new_item1))
+                items.append((new_origin2, new_item2))
+                items.sort(key=lambda x: x[1][0] * x[1][1] * x[1][2])
+
+            # Sort items by height to remove some topmost items
+            items.sort(key=lambda x: x[0][2])
+            for _ in range(self.n_samples):
+                item = items.pop()
+                bin_volume -= item[1][0] * item[1][1] * item[1][2]
+
+            self.total_volume += bin_volume
+            return items
+
         if self.n_items < 10 or self.n_items > 1000:
             raise ValueError('Number of items must be between 10 and 1000')
         
         random.seed(self.seed)
 
-        # Initialize items with a single bin
-        self.items = [([0, 0, 0], self.bin_size)]
-        
-        for _ in range(self.n_items + self.n_samples - 1):
-            (origin, item) = self.items.pop()
-            
-            # Choose the dimension with the largest size to split
-            dimension: int = item.index(max(item))
-            size: int = item[dimension]
-            
-            if size == 1:
-                self.items.append((origin, item))
-                continue
-            
-            # Randomly choose a cut point
-            cut_point: int = random.randint(1, size - 1)
-            
-            # Create 2 new items after cutting
-            new_item1: List[int] = item[:]
-            new_item2: List[int] = item[:]
-            new_item1[dimension] = cut_point
-            new_item2[dimension] = size - cut_point
-            
-            # Create 2 new origins (that is coordinates of the left-bottom-back corner)
-            new_origin1: List[int] = origin[:]
-            new_origin2: List[int] = origin[:]
-            new_origin2[dimension] += cut_point
-            
-            # Add new items to the list
-            self.items.append((new_origin1, new_item1))
-            self.items.append((new_origin2, new_item2))
-            self.items.sort(key=lambda x: x[1][0] * x[1][1] * x[1][2])
+        self.items = []
+        self.total_volume = 0
 
-        # Sort items by height to remove some topmost items
-        self.items.sort(key=lambda x: x[0][2])
-        for _ in range(self.n_samples):
-            item = self.items.pop()
-            self.volume -= item[1][0] * item[1][1] * item[1][2]
+        for bin_index in range(self.n_bins):
+            bin_origin = [bin_index * self.bin_size[0], 0, 0]
+            bin_items = generate_for_bin(bin_origin)
+            self.items.append(bin_items)
         
-        # Reorder items randomly
-        random.shuffle(self.items)
+        # Flatten the list of items and reorder randomly
+        flat_items = [item for bin_items in self.items for item in bin_items]
+        random.shuffle(flat_items)
         
         # Ensure the directory exists
         os.makedirs(os.path.dirname(f'Data/Dataset/'), exist_ok=True)
 
         # Write data to file
-        filename: str = f'Data/Dataset/{self.n_items}.{self.seed}.dat'
+        filename: str = f'Data/Dataset/{self.n_items}_{self.n_bins}_{self.seed}.dat'
         with open(filename, 'w') as file:
             file.write(f'Bin size: {self.bin_size[0]} {self.bin_size[1]} {self.bin_size[2]}\n')
-            file.write(f'Number of items: {self.n_items}\n')
-            file.write(f'Total volume of items: {self.volume}\n')
+            file.write(f'Number of bins: {self.n_bins}\n')
+            file.write(f'Number of items per bin: {self.n_items}\n')
+            file.write(f'Total volume of items: {self.total_volume}\n')
             file.write('Items:\n')
-            for (origin, item) in self.items:  # Chỉ lấy đúng số lượng vật phẩm cần thiết
+            for (_, item) in flat_items:
                 file.write(f'{item[0]} {item[1]} {item[2]}\n')
     
     def visualize(self) -> None:
@@ -127,9 +146,10 @@ class Generator:
         ax = fig.add_subplot(111, projection='3d')
 
         # Create a color palette for items
-        colors = sns.color_palette("Set3", len(self.items))
+        flat_items = [item for bin_items in self.items for item in bin_items]
+        colors = sns.color_palette("Set3", len(flat_items))
 
-        for i, (origin, item) in enumerate(self.items):
+        for i, (origin, item) in enumerate(flat_items):
             x0, y0, z0 = origin
             dx, dy, dz = item
             color = colors[i % len(colors)]
@@ -140,18 +160,19 @@ class Generator:
         ax.set_zlabel('Z')
 
         # Set limits for the axes
-        ax.set_xlim([0, self.bin_size[0]])
+        ax.set_xlim([0, self.bin_size[0] * self.n_bins])
         ax.set_ylim([0, self.bin_size[1]])
         ax.set_zlim([0, self.bin_size[2]])
 
         ax.title.set_text(f'3D Bin Packing Visualization')
-        ax.set_box_aspect(self.bin_size)
+        ax.set_box_aspect([self.bin_size[0] * self.n_bins, self.bin_size[1], self.bin_size[2]])
         
         # Add a legend with information
         info_text = (
             f'Bin size: {self.bin_size}\n'
-            f'Number of items: {self.n_items}\n'
-            f'Volume: {self.volume}'
+            f'Number of bins: {self.n_bins}\n'
+            f'Number of items per bin: {self.n_items}\n'
+            f'Total volume of items: {self.total_volume}'
         )
         plt.figtext(.75, .5, info_text, fontsize=8, ha='left', va='center', bbox=dict(facecolor='white', edgecolor='black'))
 
@@ -161,17 +182,10 @@ class Generator:
         """
         Delete the generated data file.
         """
-        filename = f'Data/Dataset/{self.n_items}.{self.seed}.dat'
+        filename = f'Data/Dataset/{self.n_items}_items_{self.n_bins}_bins_{self.seed}.dat'
         os.remove(filename)
 
 # Example of using the Generator class
-"""
-generator = Generator(200, 1, bin_size=[100, 200, 150])
-generator.generate()
-generator.visualize()
-generator.delete()
-"""
-
 for i in range(100):
-    generator = Generator(100, i, bin_size=[100, 100, 100])
+    generator = Generator(50, 5, seed=i, bin_size=[100, 100, 100])
     generator.generate()
---
+++ GeneticAlgorithm.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+"""
+We implement a genetic algorithm to solve the 3D bin packing problem.
+"""
